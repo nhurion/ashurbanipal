83d67d23ab5ac0e774841d027fe73fef63d3ce44
Nicolas Hurion <nicolas@hurion.eu>
2012-07-08 10:08:05 +0200

    Refactor to be able to launch the application from unit test

--- server/src/main/java/eu/hurion/ashurbanipal/server/Launcher.java
+++ server/src/main/java/eu/hurion/ashurbanipal/server/Launcher.java
@@ -1,8 +1,7 @@
 package eu.hurion.ashurbanipal.server;
 
+import com.bsb.common.vaadin.embed.application.EmbedVaadinApplication;
 import eu.hurion.ashurbanipal.application.AshurbanipalApplication;
-
-import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
 
 import static eu.hurion.ashurbanipal.server.MemcachedConfigurator.memcachedConfig;
 
@@ -17,22 +16,30 @@ public class Launcher {
     public static final String MEMCACHE_PASSWORD = "MEMCACHE_PASSWORD";
     public static final String MEMCACHE_SERVERS = "MEMCACHE_SERVERS";
     public static final String DEV_MODE = "DEV_MODE";
+    private static Boolean devMode;
 
     public static void main(final String[] args) {
-        EmbedVaadinForHeroku.forApplication(AshurbanipalApplication.class)
+        buildServer()
+                .openBrowser(devMode())
+                .start();
+    }
+
+    public static EmbedVaadinApplication buildServer() {
+        return EmbedVaadinForHeroku.forApplication(AshurbanipalApplication.class)
                 .withMemcachedSessionManager(devMode() ? null : memcachedConfig()
                         .username(System.getenv(MEMCACHE_USERNAME))
                         .password(System.getenv(MEMCACHE_PASSWORD))
                         .url(getMemcachedUrl()))
                 .withHttpPort(getPort())
-                .withProductionMode(!devMode())
-                .openBrowser(devMode())
-                .start();
+                .withProductionMode(!devMode());
     }
 
-    private static boolean devMode(){
-        final String stringDevMode = System.getenv(DEV_MODE);
-        return Boolean.valueOf(stringDevMode);
+    private static boolean devMode() {
+        if (devMode == null){
+            final String stringDevMode = System.getenv(DEV_MODE);
+            return Boolean.valueOf(stringDevMode);
+        }
+        return devMode;
     }
 
     private static String getMemcachedUrl() {
@@ -50,5 +57,9 @@ public class Launcher {
             return DEFAULT_PORT;
         }
         return Integer.parseInt(envPort);
+    }
+
+    public static void setDevMode(final boolean b) {
+        devMode = b;
     }
 }
